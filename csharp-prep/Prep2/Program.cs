8b8a6834fca5c31aa5015072529e466f76fb4d2c
byuimacann <mac20051@byui.edu>
2023-10-31 15:10:59 -0700

    Finished Prep 2 - Conditionals activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,77 @@
 using System;
+using System.ComponentModel.Design.Serialization;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        
+        string letter = "";
+        int lastNumber = percentage % 10;
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+            if (lastNumber < 3)
+            {
+                sign = "-";
+            }  
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+            if (lastNumber >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                sign = "-";
+            }  
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+            if (lastNumber >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                sign = "-";
+            }  
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+            if (lastNumber >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                sign = "-";
+            }  
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry...you did not pass the class. Better luck next time.");
+        }
+
     }
 }
