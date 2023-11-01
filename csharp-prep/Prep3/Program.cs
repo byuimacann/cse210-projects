3f33da4200d82c9c2dfb53612abe470267a35f02
byuimacann <mac20051@byui.edu>
2023-10-31 17:11:21 -0700

    Finished Prep 3 - Loops Activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        
+        // Console.Write("What is the magic number? ");
+        // string userInput = Console.ReadLine(); 
+        // int magicNumber = int.Parse(userInput);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess? ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
