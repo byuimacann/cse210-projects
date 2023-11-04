cfcee2c40b97be0b01986b2f51da1d0777edc1a0
byuimacann <mac20051@byui.edu>
2023-11-03 18:53:10 -0700

    Finished Prep 5 - Functions and deleted file that shouldn't have been in the folder that I put there.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Formats.Asn1;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!"); 
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int integer = number * number;
+            return integer;
+        }
+ 
+        static void DisplayResult(string userName, int integer)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {integer}");
+        }
+        
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int squared = SquareNumber(favNumber);
+        DisplayResult(name, squared);
     }
 }
